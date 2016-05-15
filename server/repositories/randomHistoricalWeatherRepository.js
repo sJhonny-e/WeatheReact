a2aad0de372f0ea51cf84181262ef627788cfe1b
sJhonny-e <sjhonny@inbox.com>
2016-05-15 13:07:45 +0100

    repository of random history data

---
+++ server/repositories/randomHistoricalWeatherRepository.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const minHigh = 15;
+const maxHigh = 25;
+const minLow = 5;
+const maxLow = 15;
+
+class RandomHistoricalWeatherRepository {
+	getHistoricalWeather(location, daysBack) {
+		var temps = [];
+		for (var i = 0; i < daysBack; i++) {
+			temps.push({
+				high: random(minHigh, maxHigh),
+				low: random(minLow, maxLow)
+			})
+		}
+
+		return temps;
+	}
+}
+
+module.exports.HistoryRepository = RandomHistoricalWeatherRepository;
+
+function random(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;	// see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+}
