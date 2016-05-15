de76e730c9ed5cc1d15aa341409ebd236d8ad949
sJhonny-e <sjhonny@inbox.com>
2016-05-15 20:52:42 +0100

    Revert "cleaning up the client app"
    This reverts commit d470bc325ed892ee0974174ff67fd4d9a7fdb6b0.

---
+++ client/app/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import SubHeader from './subheader';
+
+/**
+ * Import locally scoped styles using css-loader
+ * See style.sass in this directory.
+ *
+ * More info: https://github.com/webpack/css-loader#local-scope
+ */
+import styles from './style';
+
+/**
+ * Reference an image and get back a URL automatically via webpack.
+ * webpack takes care of versioning, bundling for production, etc.
+ */
+import logoURL from './images/react-logo.svg';
+
+const Header = () => {
+  return <header className={styles.main}>
+    <img className={styles.logo} src={logoURL} height="125"/>
+
+    <div className={styles.wrap}>
+      <h1 className={styles.title}>YARSK</h1>
+      <SubHeader>Yet Another React Starter Kit</SubHeader>
+    </div>
+  </header>;
+};
+
+Header.displayName = 'Header';
+
+export default Header;
