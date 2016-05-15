13a1783f52beefe154ea3b6398cca1c8e7ccb665
sJhonny-e <sjhonny@inbox.com>
2016-05-15 20:27:36 +0100

    added DayForecast tests

---
+++ client/app/components/DayForecast/__tests__/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import DayForecast from '../index.js';
+import styles from '../style.sass';
+import { shallow, mount } from 'enzyme';
+
+
+describe('DayForecast', function() {
+	const forecast = { desc: 'kuku', high: 5, low: 7, dayOffset: 5};
+	const averages = {high: 15, low: 12};
+
+	it('displays the component', () => {
+		const dayForecast = shallow(<DayForecast forecast={forecast} averages={averages}/>);
+
+    	expect(dayForecast.find(`.${styles.main}`)).to.have.length(1);
+	});
+
+	it('displays the children', () => {
+		const dayForecast = shallow(<DayForecast forecast={forecast} averages={averages}/>);
+
+		//has a first li with the description
+    	expect(dayForecast.find('li').first().text()).to.eql('kuku');
+    	
+    	// has 2 Temperature children with proper values
+    	expect(dayForecast.find('Temperature[type="Low"][val=7][average=12]')).to.have.length(1);
+    	expect(dayForecast.find('Temperature[type="High"][val=5][average=15]')).to.have.length(1);
+	});
+});
