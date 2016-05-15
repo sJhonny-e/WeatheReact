cc6b95f9eefb971e3b66a9a080f274d7aa5ae22c
sJhonny-e <sjhonny@inbox.com>
2016-05-15 14:24:15 +0100

    rendering an array of day forecasts

--- client/app/components/Application/index.js
+++ client/app/components/Application/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../Header';
+import DayForecast from '../DayForecast';
 
 /**
  * Import locally scoped styles using css-loader
@@ -9,16 +10,17 @@ import Header from '../Header';
  */
 import styles from './style';
 
+//fake data for now
+let forecasts = [{"dayOffset":"0","high":"16","low":"7","desc":"Partly Cloudy"},{"dayOffset":"1","high":"18","low":"8","desc":"Mostly Cloudy"},{"dayOffset":"2","high":"19","low":"11","desc":"Mostly Sunny"},{"dayOffset":"3","high":"15","low":"9","desc":"T-Storms"}];
+let averages = {"high":20,"low":9};
+
 const Application = () => {
   return <div className={styles.main}>
-    <div className={styles.wrap}>
-      <Header />
-
-      <main className={styles.body}>
-        <p>Seems like creating your own React starter kit is a rite of passage. So, here's mine.</p>
-        <p>For more information, see the <a href="https://github.com/bradleyboy/yarsk#yarsk">Readme</a>.</p>
-      </main>
-    </div>
+    { forecasts.map(f => {
+	    return <div className={styles.wrap}>
+	    	<DayForecast forecast = {f} averages= {averages}/>
+	    </div>
+    })}
   </div>;
 };
 
